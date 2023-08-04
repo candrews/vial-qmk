0253ca2afc033241de90d1e6fbc74431a6803205
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-08-03 20:42:57 -0400

    6x7 dual track buidls

--- keyboards/handwired/wylderbuilds/trackball/6x7_full_dual_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/6x7_full_dual_track/keymaps/vial/keymap.c
@@ -3,7 +3,7 @@
 #include QMK_KEYBOARD_H
 #include "6x7_full_dual_track.h"
 enum custom_layer {
-    _COLEMAK,
+//    _COLEMAK,
     _QWERTY,
     _LOWER,
     _RAISE,
@@ -14,26 +14,28 @@ enum custom_layer {
 #
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_COLEMAK] = LAYOUT_6x7(
-        KC_ESC , KC_F1   , KC_F2  , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                    KC_F7 , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12   , TO(_QWERTY),
-        DRGSCRL, KC_GRV  , KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,                    KC_6  , KC_7   , KC_8   , KC_9  , KC_0  , KC_LBRC  , KC_RBRC,
-        SNIPING, KC_TAB  , KC_Q   , KC_W  , KC_F  , KC_P  , KC_B  ,                    KC_J  , KC_L   , KC_U   , KC_Y  , KC_SCLN, KC_BSLS, KC_BACKSLASH,
-        KC_BTN3, KC_CAPS , KC_A   , KC_R  , KC_S  , KC_T  , KC_G  ,                    KC_M  , KC_N   , KC_E  ,  KC_I  , KC_O,  KC_QUOT, DRGSCRL,
-        KC_BTN1, KC_LSFT , KC_Z   , KC_X  , KC_C  , KC_D  , KC_V  ,                    KC_K  , KC_H   , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT  , KC_BTN1,
-        KC_LCTL, KC_LCTL,  KC_LGUI, KC_LBRC,KC_RBRC,                                                    KC_PLUS, KC_EQL,KC_RGUI, KC_RALT,  KC_RCTL,
-                                                RAISE , KC_SPC , KC_BSPC,     KC_LALT, KC_ENTER, LOWER,
-                                                KC_TAB, KC_HOME, KC_GRV
-        ),
+//    [_COLEMAK] = LAYOUT_6x7(
+//        KC_ESC , KC_F1   , KC_F2  , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                    KC_F7 , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12   , TO(_QWERTY),
+//        DRGSCRL, KC_GRV  , KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,                    KC_6  , KC_7   , KC_8   , KC_9  , KC_0  , KC_LBRC  , KC_RBRC,
+//        SNIPING, KC_TAB  , KC_Q   , KC_W  , KC_F  , KC_P  , KC_B  ,                    KC_J  , KC_L   , KC_U   , KC_Y  , KC_SCLN, KC_BSLS, KC_BACKSLASH,
+//        KC_BTN3, KC_CAPS , KC_A   , KC_R  , KC_S  , KC_T  , KC_G  ,                    KC_M  , KC_N   , KC_E  ,  KC_I  , KC_O,  KC_QUOT, DRGSCRL,
+//        KC_BTN1, KC_LSFT , KC_Z   , KC_X  , KC_C  , KC_D  , KC_V  ,                    KC_K  , KC_H   , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT  , KC_BTN1,
+//        KC_LCTL, KC_LCTL,  KC_LGUI, KC_LBRC,KC_RBRC,                                                    KC_PLUS, KC_EQL,KC_RGUI, KC_RALT,  KC_RCTL,
+//                                                            RAISE,                     LOWER,
+//                                                            KC_SPC,                    KC_ENTER,
+//                                                            KC_TAB, KC_GRV,   KC_RALT, KC_RGUI
+//        ),
 
     [_QWERTY] = LAYOUT_6x7(
-        KC_ESC , KC_F1   , KC_F2  , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                    KC_F7 , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12   , TO(_COLEMAK),
+        KC_ESC , KC_F1   , KC_F2  , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                    KC_F7 , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12   , KC_ESC,
         DRGSCRL, KC_GRV  , KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,                    KC_6  , KC_7   , KC_8   , KC_9  , KC_0  , KC_LBRC  , KC_RBRC,
         SNIPING, KC_TAB  , KC_Q   , KC_W  , KC_E  , KC_R  , KC_T  ,                    KC_Y  , KC_U   , KC_I   , KC_O  , KC_P  , KC_QUOT, KC_BACKSLASH,
         KC_BTN3, KC_CAPS , KC_A   , KC_S  , KC_D  , KC_F  , KC_G  ,                    KC_H  , KC_J   , KC_K   , KC_L  ,KC_SCLN, KC_RCTL, DRGSCRL,
         KC_BTN1, KC_LSFT , KC_Z   , KC_X  , KC_C  , KC_V  , KC_B  ,                    KC_N  , KC_M   , KC_COMM, KC_DOT,KC_SLSH, KC_RSFT  , KC_BTN1,
         KC_LCTL, KC_LCTL,  KC_LGUI, KC_LBRC,KC_RBRC,                                                    KC_PLUS, KC_EQL,KC_RGUI, KC_RALT,  KC_RCTL,
-                                                RAISE , KC_SPC , KC_BSPC,     KC_LALT, KC_ENTER, LOWER,
-                                                KC_TAB, KC_HOME, KC_GRV
+                                                            RAISE,                     LOWER,
+                                                            KC_SPC,                    KC_ENTER,
+                                                            KC_TAB, KC_GRV,   KC_RALT, KC_RGUI
         ),
 
     [_LOWER] = LAYOUT_6x7(
@@ -41,10 +43,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                   KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL , _______,
         _______, _______,_______,_______,_______,_______,KC_LBRC,                   KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS, _______,
         _______, _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                   KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE, _______,
-        _______, _______,_______,_______,_______,_______,_______,                   _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS, _______,
+        _______, _______,DRGSCRL,SNIPING,KC_BTN1,KC_BTN3,KC_BTN2,                   _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS, _______,
         _______, _______,_______,_______,KC_PSCR,                                                   _______, KC_P0, _______,_______, QK_BOOT,
-                                               _______,KC_BTN1,KC_BTN3,       _______,_______,_______,
-                                               _______,SNIPING,DRGSCRL
+                                                         _______,                    _______,
+                                                         KC_BTN1,                    _______,
+                                                         KC_BTN2, KC_BTN3,   _______, _______
         ),
 
     [_RAISE] = LAYOUT_6x7(
@@ -54,8 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,_______,_______,_______,_______,                   _______,_______,_______,_______,_______,KC_VOLD, _______,
         _______,_______,RGB_VAI,RGB_SAI,RGB_HUI,RGB_MOD,RGB_TOG,                   _______,_______,_______,_______,_______,_______, _______,
         QK_BOOT, _______,_______, _______,_______,                                                KC_EQL ,_______,_______,_______,_______,
-                                               _______,_______,_______,       _______,_______,_______,
-                                               _______,_______,_______
+                                                        _______,                    _______,
+                                                        _______,                    _______,
+                                                        _______, _______,   _______, _______
         )
 };
 
@@ -77,26 +81,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // DEFINE THESE KEYS SOMEWHERE ON THE KEYMAP, USUALLY LEFT BOTTOM ROW
 //           _______,RGB_VAI,RGB_SAI,RGB_HUI,RGB_MOD,RGB_TOG,
 
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+    } else if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_PGUP);
+        } else {
+            tap_code(KC_PGDN);
+        }
+    }
+    return false;
+}
+#endif
+
 #ifdef RGBLIGHT_ENABLE
 const rgblight_segment_t PROGMEM colemak_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {9, 3, HSV_PURPLE},       // Light 4 LEDs, starting with LED 6
-    {21, 3, HSV_PURPLE}      // Light 4 LEDs, starting with LED 6
+    {15, 3, HSV_PURPLE},       // Light 4 LEDs, starting with LED 6
+    {33, 3, HSV_PURPLE}      // Light 4 LEDs, starting with LED 6
 );
 const rgblight_segment_t PROGMEM querty_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {9, 3, HSV_RED},       // Light 4 LEDs, starting with LED 6
-    {21, 3, HSV_RED}      // Light 4 LEDs, starting with LED 6
+    {15, 3, HSV_RED},       // Light 4 LEDs, starting with LED 6
+    {33, 3, HSV_RED}      // Light 4 LEDs, starting with LED 6
 );
 const rgblight_segment_t PROGMEM lower_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {9, 3, HSV_BLUE},
-    {21, 3, HSV_BLUE}
+    {15, 3, HSV_BLUE},
+    {33, 3, HSV_BLUE}
 );
 const rgblight_segment_t PROGMEM raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {9, 3, HSV_YELLOW},
-    {21, 3, HSV_YELLOW}
+    {15, 3, HSV_YELLOW},
+    {33, 3, HSV_YELLOW}
 );
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    colemak_layer,
+//    colemak_layer,
     querty_layer,    // Overrides caps lock layer
     lower_layer,    // Overrides other layers
     raise_layer     // Overrides other layers
@@ -108,14 +131,15 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 //}
 
 layer_state_t default_layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(0, layer_state_cmp(state, _COLEMAK));
+//    rgblight_set_layer_state(0, layer_state_cmp(state, _COLEMAK));
+    rgblight_set_layer_state(0, layer_state_cmp(state, _QWERTY));
     return state;
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(1, layer_state_cmp(state, _QWERTY));
-    rgblight_set_layer_state(2, layer_state_cmp(state, _LOWER));
-    rgblight_set_layer_state(3, layer_state_cmp(state, _RAISE));
+//    rgblight_set_layer_state(1, layer_state_cmp(state, _QWERTY));
+    rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
     return state;
 }
 
@@ -189,9 +213,9 @@ bool oled_task_user(void) {
         oled_write_P(PSTR("Layer\n"), false);
 
         switch (get_highest_layer(layer_state)) {
-            case _COLEMAK:
-                oled_write_P(PSTR("COLMK\n"), false);
-                break;
+//            case _COLEMAK:
+//                oled_write_P(PSTR("COLMK\n"), false);
+//                break;
             case _QWERTY:
                 oled_write_P(PSTR("QWRTY\n"), false);
                 break;
