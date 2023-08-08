37b57a51f72ece2587df32e38345c7acd2b3eef9
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-08-08 18:48:42 -0400

    Updated 3x6_3 with rgb underglow

--- keyboards/handwired/wylderbuilds/trackball/3x6_3_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/3x6_3_track/keymaps/vial/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     RAISE,   KC_SPC,  KC_TAB,          KC_LCTRL, KC_ENT, LOWER
         ),
     [_RAISE] = LAYOUT_split_3x6_3(
-        _______,_______,_______,_______,_______,KC_LBRC,                    KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
+        RGB_VAI,RGB_SAI,RGB_HUI,RGB_MOD,RGB_TOG,KC_LBRC,                    KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
         QK_BOOT,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                    KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
         _______,DRGSCRL,SNIPING,KC_BTN3,KC_BTN2,KC_BTN1,                    KC_EQL, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
                                 _______,   _______,  _______,          _______, _______, _______
@@ -30,37 +30,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 _______,   _______,  _______,         _______, _______, _______
         )
 };
-
-// STANDARD 3-KEY TRACKBALL CLUSTER ON RIGHT
-//const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//    [_QWERTY] = LAYOUT_split_3x5_3(
-//        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-//        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-//        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-//                                      RAISE, KC_SPC, KC_BSPC,      LOWER,
-//                                                                   KC_ENT,
-//                                                                KC_LALT
-//        ),
-//
-//    [_LOWER] = LAYOUT_split_3x5_3(
-//        _______,_______,_______,_______,KC_LBRC,                     KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,
-//        KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                     KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,
-//        _______,_______,_______,_______,SNIPING,                     _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL,
-//                                      KC_BTN3, KC_BTN1,DRGSCRL,    _______,
-//                                                                   _______,
-//                                                                QK_BOOT
-//
-//        ),
-//
-//    [_RAISE] = LAYOUT_split_3x5_3(
-//        KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                     KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,
-//        _______,_______,_______,_______,KC_LBRC,                     KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,
-//        KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                     KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,
-//                                        _______,_______,QK_BOOT,   _______,
-//                                                                   _______,
-//                                                                 _______
-//        )
-//};
 
 
 #ifdef OLED_ENABLE
@@ -107,8 +76,57 @@ bool oled_task_user(void) {
 }
 #endif
 
+#ifdef RGBLIGHT_ENABLE
+const rgblight_segment_t PROGMEM querty_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {9, 3, HSV_RED},       // Light 4 LEDs, starting with LED 6
+    {21, 3, HSV_RED}      // Light 4 LEDs, starting with LED 6
+);
+const rgblight_segment_t PROGMEM lower_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {9, 3, HSV_BLUE},
+    {21, 3, HSV_BLUE}
+);
+const rgblight_segment_t PROGMEM raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {9, 3, HSV_YELLOW},
+    {21, 3, HSV_YELLOW}
+);
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    querty_layer,    // Overrides caps lock layer
+    lower_layer,    // Overrides other layers
+    raise_layer     // Overrides other layers
+);
+
+//bool led_update_user(led_t led_state) {
+//    rgblight_set_layer_state(0, led_state.caps_lock);
+//    return true;
+//}
+
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(0, layer_state_cmp(state, _QWERTY));
+    return state;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
+    return state;
+}
+
+
+#endif
+
 
 void keyboard_post_init_user(void) {
+#ifdef RGBLIGHT_ENABLE
+        // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+
+//    rgblight_layers = my_rgb_layers;
+//    rgblight_enable_noeeprom(); // Enables RGB, without saving settings
+//    rgblight_sethsv_noeeprom_cyan();
+//    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+#endif
+
 #ifdef CONSOLE_ENABLE
     debug_enable=true;
     debug_matrix=true;
@@ -121,5 +139,6 @@ void keyboard_post_init_user(void) {
     debug_mouse=false;
 #endif
 }
+
 
 
